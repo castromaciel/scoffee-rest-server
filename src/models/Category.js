bfdc37eb5644cf95f33eb920490a8d7fb69758c2
castromaciel <castromaciel00@gmail.com>
2023-03-19 20:27:18 -0300

    feat: create category schema

---
+++ src/models/Category.js
@@ -0,0 +1,20 @@
+import { model, Schema } from 'mongoose'
+
+const CategorySchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is requireds']
+  },
+  status: {
+    type: Boolean,
+    default: true,
+    required: [true, 'Status is required']
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: [true, 'User is required']
+  }
+})
+
+export default model('Category', CategorySchema)
