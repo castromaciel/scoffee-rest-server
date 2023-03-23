dbfe0a680cdae804a7d232014250035f9e39cbc0
castromaciel <castromaciel00@gmail.com>
2023-03-23 19:58:18 -0300

    feat: add category controllers

--- src/controllers/02-categories.js
+++ src/controllers/02-categories.js
@@ -1,5 +1,55 @@
 import { request, response } from 'express'
-import { Category } from '../models/index.js'
+import { Category, User } from '../models/index.js'
+
+export const getCategories = async (req, res) => {
+  const { limit = 20 } = req.query
+  const queryFilter = {
+    status: true
+  }
+
+  const [total, categories] = await Promise.all([
+    Category.count(),
+    Category.find(queryFilter)
+      .populate('user', ['name', 'username', 'email'], User)
+      .limit(Number(limit))
+  ])
+
+  return res.json({
+    headers: {
+      status: 'success',
+      message: 'Categories retrieved successfully',
+      timestamp: new Date().toISOString()
+    },
+    total,
+    categories
+  })
+}
+
+export const getCategory = async (req, res) => {
+  const { id } = req.params
+  const category = await Category.findById(id)
+    .populate('user', ['name', 'username'], User)
+
+  if (!category) {
+    return res.status(404).json({
+      headers: {
+        status: 'error',
+        message: 'Category not found'
+      },
+      categories: []
+    })
+  }
+
+  return res.json({
+    headers: {
+      message: 'Categories retrieved successfully',
+      timestamp: new Date().toISOString()
+    },
+    categories: [
+      category
+    ]
+  })
+}
 
 export const createCategory = async (req = request, res = response) => {
   const name = req.body.name.toUpperCase()
@@ -9,6 +59,7 @@ export const createCategory = async (req = request, res = response) => {
   if (categoryDb) {
     return res.status(400).json({
       headers: {
+        status: 'error',
         message: `Category '${name}', already exists`
       }
     })
@@ -24,7 +75,28 @@ export const createCategory = async (req = request, res = response) => {
 
   return res.status(201).json({
     headers: {
+      status: 'success',
       message: `Category '${name}', created successfully`
     }
+  })
+}
+
+export const updateCategory = async (req, res) => {
+  const { id } = req.params
+  const {
+    _id, status, ...rest
+  } = req.body
+
+  const category = await Category.findByIdAndUpdate(id, rest)
+    .populate('user', ['name', 'username', 'email'], User)
+
+  return res.status(200).json({
+    headers: {
+      status: 'success',
+      message: `Category '${category.name}', updated successfully`
+    },
+    categories: [
+      category
+    ]
   })
 }
