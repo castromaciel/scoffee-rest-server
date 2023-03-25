f23cda129c5c656244e8ce366e356963e9d5d72a
castromaciel <castromaciel00@gmail.com>
2023-03-24 21:11:17 -0300

    feat: add middlewares

--- src/routes/02-categories.routes.js
+++ src/routes/02-categories.routes.js
@@ -1,28 +1,42 @@
 import { Router } from 'express'
 import { check } from 'express-validator'
-import { ROLES } from '../constants/index.js'
-import { createCategory } from '../controllers/02-categories.js'
-import { hasRoles, validateFields, validateToken } from '../middlewares/index.js'
+import {
+  createCategory, getCategories, getCategory, updateCategory
+} from '../controllers/02-categories.js'
+import { existCategory, isCategoryIdExist } from '../database/db-validators.js'
+import { validateFields, validateToken } from '../middlewares/index.js'
 
 const router = Router()
 
-router.get('/', (req, res) => {
-  res.json('GET todo ok')
-})
+router.get('/', getCategories)
 
-router.get('/:id', (req, res) => {
-  res.json('GET una categoria')
-})
+router.get(
+  '/:id',
+  [
+    check('id', 'Id is not valid').isMongoId()
+  ],
+  getCategory
+)
 
-router.post('/', [
-  validateToken,
-  check('name', 'Name is required').not().isEmpty(),
-  validateFields
-], createCategory)
+router.post(
+  '/',
+  [
+    validateToken,
+    check('name', 'Name is required').not().isEmpty(),
+    check('name').custom(existCategory),
+    validateFields
+  ],
+  createCategory
+)
 
-router.put('/', (req, res) => {
-  res.json('PUT')
-})
+router.put(
+  '/:id',
+  [
+    check('id', 'Id is not valid').isMongoId(),
+    check('id').custom(isCategoryIdExist)
+  ],
+  updateCategory
+)
 
 router.delete('/', (req, res) => {
   res.json('DELETE')
