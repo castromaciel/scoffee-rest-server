075ece022e6238a21a4d1a0548989f738e12c7d1
castromaciel <castromaciel00@gmail.com>
2023-04-09 21:56:15 -0300

    feat: add search controller

--- src/controllers/04-search.js
+++ src/controllers/04-search.js
@@ -1,11 +1,62 @@
+/* eslint-disable consistent-return */
 import { request, response } from 'express'
+import { isValidObjectId } from 'mongoose'
+import { User } from '../models/index.js'
 
-export const search = (req = request, res = response) => {
+const availableCollections = [
+  'users',
+  'categories',
+  'products'
+]
+
+const searchUsers = async (term = '', res = response) => {
+  if (isValidObjectId(term)) {
+    const users = await User.findById(term)
+
+    return res.json({
+      results: {
+        users: users || []
+      }
+    })
+  }
+
+  return res.status(404).json({
+    message: 'Empty collection',
+    results: []
+  })
+}
+
+export const search = async (req = request, res = response) => {
   const { collection, term } = req.params
 
-  res.json({
-    collection,
-    term,
-    message: 'Search..'
-  })
+  if (!availableCollections.includes(collection)) {
+    return res.status(400).json({
+      message: `${collection} not available`
+    })
+  }
+
+  switch (collection) {
+    case 'users':
+      await searchUsers(term, res)
+      break
+
+    case 'categories':
+      return res.json({
+        collection,
+        term,
+        message: 'Search..'
+      })
+
+    case 'products':
+      return res.json({
+        collection,
+        term,
+        message: 'Search..'
+      })
+
+    default:
+      return res.status(500).json({
+        message: 'Search is not available'
+      })
+  }
 }
